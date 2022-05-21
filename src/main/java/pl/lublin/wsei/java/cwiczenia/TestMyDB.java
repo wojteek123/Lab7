b45b39d7cd333d6cdb6c3d7a3496ce7f7b680f79
wojtek123 <1889wojtek@gmail.com>
2022-05-21 20:02:29 +0200

    ukonczenie pkt 7 /lab7

--- src/main/java/pl/lublin/wsei/java/cwiczenia/TestMyDB.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/TestMyDB.java
@@ -1,6 +1,6 @@
 package pl.lublin.wsei.java.cwiczenia;
 
-import java.sql.Connection;
+import java.sql.*;
 
 public class TestMyDB {
     public static void main(String[] args) {
@@ -10,6 +10,23 @@ public class TestMyDB {
         Connection conn= dataBase.getConnection();
         if(conn!=null){
             System.out.printf("udalo sie");
+            try {
+                Statement polecenie = conn.createStatement();
+                Number dostawa = 100;
+                polecenie.executeUpdate("UPDATE coffee_houses SET COFFEE= COFFEE+"+dostawa+" WHERE CITY ='SF' ");
+
+                ResultSet res = polecenie.executeQuery("SELECT * FROM coffee_houses");
+                System.out.printf("%10s%20s%7s%7s%7s \n", "STORE_ID", "CITY", "COFFEE", "MERCH", "TOTAL");
+                while (res.next()) {
+                    System.out.printf("%10d%20s%7d%7d%7d \n", res.getInt(1), res.getString("CITY"), res.getInt(3), res.getInt(4), res.getInt(5));
+                }
+
+            }catch(SQLException e){
+                System.out.printf("blad w poleceniu");
+            }
+
+
+
         }
         dataBase.closeConnection();
         System.out.printf("polacznie zakoncvzone");
